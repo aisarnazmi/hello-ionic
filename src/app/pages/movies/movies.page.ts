a91190a6dabac6c605c98ef2c0e448735066174d
Aisar Nazmi <aisar.nazmiey39@gmail.com>
2023-03-20 09:02:45 +0800

    add trending movies page & api call methods

---
+++ src/app/pages/movies/movies.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular';
+
+import { environment } from 'src/environments/environment';
+import { MovieService } from 'src/app/services/movie.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.page.html',
+  styleUrls: ['./movies.page.scss'],
+})
+export class MoviesPage implements OnInit {
+  movies:Array<any> = [];
+  current_page = 1;
+  imageBaseUrl = environment.imageUrl;
+
+  constructor(
+    private movieServie: MovieService,
+    private loadingCtrl: LoadingController
+  ) {}
+
+  ngOnInit() {
+    this.loadMovies();
+  }
+
+  async loadMovies(event?: InfiniteScrollCustomEvent) {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading..',
+      spinner: 'bubbles',
+    });
+    await loading.present();
+
+    this.movieServie.getTopRatedMovies(this.current_page).subscribe((res) => {
+      // this.movies = [...this.movies, ...res.results];
+      loading.dismiss();
+      this.movies.push(...res.results);
+      
+      event?.target.complete();
+
+      if (event) {
+        event.target.disabled = res.total_pages === this.current_page;
+      }
+    });
+  }
+
+  loadMore(event: InfiniteScrollCustomEvent) {
+    this.current_page++;
+    this.loadMovies(event);
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apikey: 'a97243d7813d31446f6c43284e6854d5',
+  baseUrl: 'https://api.themoviedb.org/3',
+  imageUrl: 'http://image.tmdb.org/t/p'
 };
 
 /*
